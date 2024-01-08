bfd6801dda7bf808c5766ad2debeda2a241a9881
danwan1027 <danwan1027@gmail.com>
2024-01-09 00:12:25 +0800

    feat: yasmine 的 backend

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from filterData.trendingData import netflix_data
 from gsheet import get_sheet
+import filterData.styleData as styleData
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
-# SAMPLE_SPREADSHEET_ID = "1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY"
+SAMPLE_SPREADSHEET_ID = "1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY"
 # # SAMPLE_SPREADSHEET_ID = ""
 # SAMPLE_RANGE_NAME = "TV_Shows"
 # SAMPLE_RANGE_NAME = ""
@@ -25,8 +26,25 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 app = Flask(__name__)
 
+def init():
+    SAMPLE_RANGE_NAME = "netflix_titles"
+    global df_netflix_titles
+    df_netflix_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    SAMPLE_RANGE_NAME = "disney_plus_titles"
+    global df_disney_plus_titles
+    df_disney_plus_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    SAMPLE_RANGE_NAME = "amazon_prime_titles"
+    global df_amazon_prime_titles
+    df_amazon_prime_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    SAMPLE_RANGE_NAME = "hulu_titles"
+    global df_hulu_titles
+    df_hulu_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    return None
+
+
 @app.route("/")
 def main():
+    init()
     fruits = ['apple', 'orange', 'pear', 'pineapple', 'durian']
     return render_template('index.html', fruits=fruits)
 
@@ -48,11 +66,24 @@ def example3():
 # Yasmine
 @app.route("/style")
 def style():
-    return render_template('vedioStyle/vedioStyle.html')
+    listin_netflix_json, listin_disney_json, listin_amazon_json, listin_hulu_json = styleData.list_count()
+    netflix_movie_tv_count_json, disney_movie_tv_count_json, amazon_movie_tv_count_json, hulu_movie_tv_count_json = styleData.movies_shows_count()
+    season_count_json = styleData.one_season_count()
+    return render_template('vedioStyle/vedioStyle.html', 
+                           listin_netflix_json=listin_netflix_json, 
+                           listin_disney_json=listin_disney_json, 
+                           listin_amazon_json=listin_amazon_json, 
+                           listin_hulu_json=listin_hulu_json, 
+                           netflix_movie_tv_count_json=netflix_movie_tv_count_json, 
+                           disney_movie_tv_count_json=disney_movie_tv_count_json, 
+                           amazon_movie_tv_count_json=amazon_movie_tv_count_json, 
+                           hulu_movie_tv_count_json=hulu_movie_tv_count_json,
+                           season_count_json=season_count_json)
 
 # 廖老大
 @app.route("/rating")
 def rating():
+    styleData.movies_shows_count()
     return render_template('vedioRating/vedioRating.html')
 
 # 睿弘
@@ -92,57 +123,3 @@ def add():
 
     return render_template('index.html')
 
-
-# def get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME):
-#     """Shows basic usage of the Sheets API.
-#     Prints values from a sample spreadsheet.
-#     """
-
-#     creds = None
-#     # The file token.json stores the user's access and refresh tokens, and is
-#     # created automatically when the authorization flow completes for the first
-#     # time.
-#     if os.path.exists("token.json"):
-#         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-#     # If there are no (valid) credentials available, let the user log in.
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 "credentials.json", SCOPES
-#             )
-#             creds = flow.run_local_server(port=3000)
-#         # Save the credentials for the next run
-#         with open("token.json", "w") as token:
-#             token.write(creds.to_json())
-
-#     try:
-#         service = build("sheets", "v4", credentials=creds)
-
-#         # Call the Sheets API
-#         sheet = service.spreadsheets()
-#         result = (
-#             sheet.values()
-#             .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-#             .execute()
-#         )
-#         values = result.get("values", [])
-#         if not values:
-#             print("No data found.")
-#         else:
-#             print("Data found.")
-#             df = pd.DataFrame(values)
-#             # Get the first row for the header
-#             df.columns = df.iloc[0]
-#             # Take the data less the header row
-#             df = df[1:]
-#             # print the numbrr of rows
-#             print(df.head())
-            
-
-
-#     except HttpError as err:
-#         print(err)
-
-#     return df
