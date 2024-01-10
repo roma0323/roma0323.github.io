3a79d80ee8f95f7d99cfee695743e73e3ab9422f
anjrlee <102264715+anjrlee@users.noreply.github.com>
2024-01-10 11:53:28 +0800

    add:random pick

--- app.py
+++ app.py
@@ -1,31 +1,56 @@
 from flask import Flask, render_template
 import os.path
-
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from flask import request
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from filterData.trendingData import make_bar_chart_race
+from filterData.trendingData import get_quantity_by_year
+from gsheet import get_sheet
+import filterData.styleData as styleData
+import random
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY"
-SAMPLE_RANGE_NAME = "Sheet1!A1"
-
+SAMPLE_RANGE_NAME = "TV_Shows"
+sheet = None
+df_netflix_titles = None
 
 app = Flask(__name__)
+
+def init():
+    SAMPLE_RANGE_NAME = "netflix_titles"
+    global df_netflix_titles
+    if df_netflix_titles is None:
+        df_netflix_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+        SAMPLE_RANGE_NAME = "disney_plus_titles"
+        global df_disney_plus_titles
+        df_disney_plus_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+        SAMPLE_RANGE_NAME = "amazon_prime_titles"
+        global df_amazon_prime_titles
+        df_amazon_prime_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+        SAMPLE_RANGE_NAME = "hulu_titles"
+        global df_hulu_titles
+        df_hulu_titles = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    return None
+
+
 @app.route("/")
 def main():
-    fruits = ['apple', 'orange', 'pear', 'pineapple', 'durian']
-    return render_template('index.html', fruits=fruits)
+    init()
+    return render_template('index.html')
 
 # 範例一
-
-@app.route("/form")
-def form():
-    return render_template('form.html')
 @app.route("/example1")
 def example1():
     return render_template('publisherAnalysis.html')
@@ -43,31 +68,148 @@ def example3():
 # Yasmine
 @app.route("/style")
 def style():
-    return render_template('vedioStyle/vedioStyle.html')
+    listin_netflix_json, listin_disney_json, listin_amazon_json, listin_hulu_json = styleData.list_count()
+    netflix_movie_tv_count_json, disney_movie_tv_count_json, amazon_movie_tv_count_json, hulu_movie_tv_count_json = styleData.movies_shows_count()
+    season_count_json = styleData.one_season_count()
 
-# 廖老大
-@app.route("/rating")
-def rating():
-    return render_template('vedioRating/vedioRating.html')
+    return render_template('vedioStyle/vedioStyle.html', 
+                           listin_netflix_json=listin_netflix_json, 
+                           listin_disney_json=listin_disney_json, 
+                           listin_amazon_json=listin_amazon_json, 
+                           listin_hulu_json=listin_hulu_json, 
+                           netflix_movie_tv_count_json=netflix_movie_tv_count_json, 
+                           disney_movie_tv_count_json=disney_movie_tv_count_json, 
+                           amazon_movie_tv_count_json=amazon_movie_tv_count_json, 
+                           hulu_movie_tv_count_json=hulu_movie_tv_count_json,
+                           season_count_json=season_count_json)
+
 
 # 睿弘
 @app.route("/trending")
 def trending():
-    return render_template('vedioTrending/vedioTrending.html')
+    hulu_date_list,hulu_number_list=get_quantity_by_year("hulu_titles")
+    netflix_date_list,netflix_number_list=get_quantity_by_year("netflix_titles")
+    return render_template('vedioTrending/vedioTrending.html',
+                           hulu_date_list=hulu_date_list,
+                           hulu_number_list=hulu_number_list,
+                           netflix_date_list=netflix_date_list,
+                           netflix_number_list=netflix_number_list)
+
+
+# 廖老大
+@app.route("/rating")
+def rating():
+    SAMPLE_SPREADSHEET_ID = "1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY"
+    SAMPLE_RANGE_NAME = "TV_Shows"
+    global sheet
+    if sheet is None:
+        sheet = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME) 
+
+    
+    return render_template('vedioRating/vedioRating.html',
+                            Netflix_IMDb_rate = write_in_js(sheet,'Netflix'),
+                            Hulu_IMDb_rate = write_in_js(sheet,'Hulu'),
+                            Prime_IMDb_rate = write_in_js(sheet,'Prime Video'),
+                            Disney_IMDb_rate = write_in_js(sheet,'Disney+'))
+
+def write_in_js(sheet,OTT_platform):
+     # filtered_data = sheet[sheet['IMDb']!= '']
+    
+
+    filtered_data = sheet[(sheet[OTT_platform] == '1') & (sheet['IMDb']!= '')]
+
+    # Extract the values from the "IMDb" column that satisfy the conditions
+    imdb_list = filtered_data['IMDb'].tolist()
+
+    number_list = [float(num) for num in imdb_list]
+
+    average_rate = round(sum(number_list) / len(number_list),2)
+    print(f'{OTT_platform} vedio average IMDb rate is:', average_rate)
+
+
+    import json
+
+    # Convert the Python list to JSON string
+    json_data = json.dumps(imdb_list)
+    variable_exists = False
+
+    if OTT_platform=='Prime Video':
+        OTT_platform='prime_video'
+    if OTT_platform=='Disney+':
+        OTT_platform='Disney'    
+
+    with open('./static/assets/js/data.js', 'r') as js_file:
+        for line in js_file:
+            if f'const {OTT_platform}_list =' in line:
+                variable_exists = True
+                break
+                
+    if not variable_exists:
+        # Writing JSON data to a file
+        with open('./static/assets/js/data.js', 'a') as js_file:
+            js_file.write('\n')
+            js_file.write(f'const {OTT_platform}_list = ')
+            js_file.write(json_data)
+            js_file.write(';')
+
+    return average_rate        
+
+# 李安之
+@app.route("/perference")
+def perference():
+    return render_template('forms/perferForm.html')
+
+@app.route("/addForm")
+def addForm():
+    return render_template('forms/addForm.html')
+
+@app.route("/addAmazonForm")
+def addAmazonForm():
+    return render_template('forms/addAmazonForm.html')
+
 
 # Test connect to google sheet
 @app.route("/test")
 def test():
+    SAMPLE_SPREADSHEET_ID = "1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY"
+    SAMPLE_RANGE_NAME = "TV_Shows"
+    get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    return render_template('index.html')
+
+
+# Test add data to google sheet
+@app.route("/add", methods=['POST'])
+def add():
+    if request.method=='POST':
+        form_data=request.form
+        print(form_data)
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name('credentials_for_update.json', scope)
+        client = gspread.authorize(creds)
+        sheet = client.open('Datavisualization')
+        worksheet = sheet.get_worksheet(0)  # Change the index to your desired worksheet
+        platformData=["Netflix","Hulu","Prime Video","Disney+"]
+        platformDataToAdd=[0,0,0,0]
+        for i in range (4):
+            if platformData[i]==form_data['platform']:
+                platformDataToAdd[i]=1
+                break
+        data_to_add = ["", form_data['filmName'],form_data['year'],form_data['age'],form_data['IMDB'],form_data['RottenTomatoes'],platformDataToAdd[0],platformDataToAdd[1],platformDataToAdd[2],platformDataToAdd[3]]
+        worksheet.append_row(data_to_add)
+        return render_template('index.html',confirm="上傳成功")
+    return render_template('index.html')
+
+
+@app.route("/perferGet",methods=['POST'])
+def perferGet():
+     
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
+    df = pd.DataFrame()
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -76,7 +218,6 @@ def test():
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=3000)
-        # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
@@ -85,15 +226,42 @@ def test():
 
         # Call the Sheets API
         sheet = service.spreadsheets()
-        valueData = [["Apple", "Orange", "Banana"]]
         result = (
             sheet.values()
-            .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED", body={"values": valueData})
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
             .execute()
         )
+        values = result.get("values", [])
+        if not values:
+            print("No data found.")
+        else:
+            print("Data found.")
+            df = pd.DataFrame(values)
+            # Get the first row for the header
+            df.columns = df.iloc[0]
+            # Take the data less the header row
+            df = df[1:]
+            # print the numbrr of rows
+        if request.method=='POST':
+            form_data=request.form
+            platformData=["Netflix","Hulu","Prime Video","Disney+"]
+            ageData=["7+","16+","18+"]
+            temp = df[
+                (pd.to_numeric(df['IMDb'], errors='coerce') >= float(form_data['IMDB']))
+            ]
+            for i in range(4):
+                if form_data['platform']==platformData[i]:
+                    temp=temp[temp[platformData[i]]=='1']
+            for i in range(3):
+                if form_data['age']==ageData[0]:
+                    temp=temp[temp['Age']==ageData[0]]
+                elif form_data['age']==ageData[1]:
+                    temp=temp[(temp['Age']==ageData[0])|(temp['Age']==ageData[1])]
+            #print(temp.iloc[0])
+            
 
-
+            
     except HttpError as err:
         print(err)
-        
-    return render_template('index.html')
+    row, col = temp.shape
+    return render_template('forms/recommondation.html',val=(temp.iloc[random.randint(0,row)]['Title']))
---
+++ filterData/trendingData.py
@@ -0,0 +1,114 @@
+from gsheet import get_sheet
+from datetime import datetime
+import plotly.express as px
+import pandas as pd
+# import streamlit as st
+
+def get_quantity_by_year(OTT_platform):
+    ott_date_data = get_OTT_data("1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY",OTT_platform)   #get hulu date data
+
+
+    from collections import Counter
+
+    # Count occurrences of each date
+    date_counts = Counter(ott_date_data)
+
+    # Extract unique dates and their counts
+    date_list = list(date_counts.keys())
+    number_list = list(date_counts.values())
+
+    print("date_list =", date_list)
+    print("number_list =", number_list)
+    return date_list,number_list
+
+
+def make_bar_chart_race():
+    # netflix_date = get_OTT_data("1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY","netflix_titles")   #get netflix date data
+    # # disney_plus_date = get_OTT_data("1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY","disney_plus_titles")   #get disney_plus date data
+    # hulu_date = get_OTT_data("1kd8gloSg7VE0aXvaSzBOqVDAX3nKfRXsoQBLZz_i1zY","hulu_titles")   #get hulu date data
+    
+
+    # hulu_OTT = ['hulu'] * len(hulu_date[:1100])
+    # hulu_quantity = [1] * len(hulu_date[:1100])
+    # hulu_quantity = [i* + 1 for i in range(len(hulu_quantity))]
+    
+    # # disney_plus_OTT = ['disney_plus'] * len(disney_plus_date)
+    # # disney_plus_quantity = [1] * len(disney_plus_date)
+    # # disney_plus_quantity = [i* + 1 for i in range(len(disney_plus_quantity))]
+
+    # netflix_OTT = ['netflix'] * len(netflix_date[:5000])
+    # netflix_quantity = [1] * len(netflix_date[:5000])
+    # netflix_quantity = [i* + 1 for i in range(len(netflix_quantity))]
+    
+
+    # static_dates = hulu_date[:1100]+netflix_date[:5000]
+    # static_OTT = hulu_OTT+netflix_OTT
+    # static_quantity = hulu_quantity+netflix_quantity
+
+    # # static_dates = hulu_date+disney_plus_date+netflix_date
+    # # static_OTT = hulu_OTT+disney_plus_OTT+netflix_OTT
+    # # static_quantity = hulu_quantity+disney_plus_quantity+netflix_quantity
+
+
+  
+
+    
+    # # Create a DataFrame with static values
+    # data = {
+    #     'OTT': static_OTT,
+    #     'quantity': static_quantity,
+    #     'Date': pd.to_datetime(static_dates)
+    # }
+    # df = pd.DataFrame(data)
+
+    # # Store each column in a seperate varibale.
+    # OTT = df["OTT"]
+    # quantity = df["quantity"]
+    # date = df["Date"].dt.strftime("%Y-%m-%d")
+
+
+
+    # # Create Animated Bar Chart and store figure as fig
+    # fig = px.bar(
+    #     df,
+    #     x=OTT,
+    #     y=quantity,
+    #     color=OTT,
+    #     animation_frame=date,
+    #     animation_group=OTT,
+    #     range_y=[0, 2000],
+    #     color_discrete_map={'netflix': '#e50914', 'hulu': '#1CE783'}
+
+    # )
+
+    # # # Save Chart and export to HTML
+    # plotly.offline.plot(fig, filename="hold3.html")
+    return None
+
+def get_OTT_data(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME):
+
+    sheet_data = get_sheet(SAMPLE_SPREADSHEET_ID,SAMPLE_RANGE_NAME)
+    converted_dates = []
+
+    for date in sheet_data['date_added']:
+        if date:  # 检查是否为空字符串
+            try:
+                dt = datetime.strptime(date, '%d-%b-%y')
+                new_date = dt.replace(day=1)
+                new_format = new_date.strftime('%Y-%m-%d')
+                converted_dates.append(new_format)
+            except ValueError:
+                try:
+                    # If the original format fails, try parsing with a different format
+                    dt = datetime.strptime(date, '%B %d, %Y')
+                    new_date = dt.replace(month=1,day=1)
+                    new_format = new_date.strftime('%Y-%m-%d')
+                    converted_dates.append(new_format)
+                except ValueError:
+                    print(f"Ignoring invalid date format: {date}")
+                    continue
+        else:
+            print("Encountered an empty date string.")
+            
+    return converted_dates
+
